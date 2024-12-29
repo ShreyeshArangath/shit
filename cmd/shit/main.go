f61216a0f443bd4bfc49cf3d26b74485bf4e2039
Shreyesh Shaju Arangath <sarangath@linkedin.com>
2024-12-29 07:41:17 +0300

    do not generate docs everytime

--- cmd/shit/main.go
+++ cmd/shit/main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/ShreyeshArangath/shit/internal/git/plumbing"
 	"github.com/ShreyeshArangath/shit/internal/git/porcelain"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	"github.com/spf13/cobra/doc"
 )
 
 var log = logrus.New()
@@ -41,14 +39,14 @@ func init() {
 	rootCmd.AddCommand(porcelain.GetCheckoutCmd())
 	rootCmd.AddCommand(plumbing.GetShowRefCmd())
 	// Create the directory to store the documentation
-	err := os.MkdirAll("docs", 0755)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// err := os.MkdirAll("docs", 0755)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	// Generate the Markdown documentation
-	err = doc.GenMarkdownTree(GetRootCmd(), "docs")
-	if err != nil {
-		log.Fatal(err)
-	}
+	// // Generate the Markdown documentation
+	// err = doc.GenMarkdownTree(GetRootCmd(), "docs")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 }
